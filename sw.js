1dfda4adc9811f3b6e4dd863a1675131fd32898f
Renan LE CARO <renan.lecaro@gmail.com>
2017-06-14 00:19:54 +0200

    Removed disfunctional sw, and merged point color and size picker

--- sw.js
+++ sw.js
@@ -1,49 +1,49 @@
-// Chrome's currently missing some useful cache methods,
-// this polyfill adds them.
-importScripts('serviceworker-cache-polyfill.js');
+// // Chrome's currently missing some useful cache methods,
+// // this polyfill adds them.
+// importScripts('serviceworker-cache-polyfill.js');
 
-// Here comes the install event!
-// This only happens once, when the browser sees this
-// version of the ServiceWorker for the first time.
-self.addEventListener('install', function(event) {
-    // We pass a promise to event.waitUntil to signal how 
-    // long install takes, and if it failed
-    event.waitUntil(// We open a cache…
-    caches.open('simple-sw-v1').then(function(cache) {
-        // And add resources to it
-        return cache.addAll(['./index.html', './styles.css', "./logo-min.svg"]);
-    }));
-});
-
-// // The fetch event happens for the page request with the
-// // ServiceWorker's scope, and any request made within that
-// // page
-// self.addEventListener('fetch', function(event) {
-//   // Calling event.respondWith means we're in charge
-//   // of providing the response. We pass in a promise
-//   // that resolves with a response object
-//   event.respondWith(
-//     // First we look for something in the caches that
-//     // matches the request
-//     caches.match(event.request).then(function(response) {
-//       // If we get something, we return it, otherwise
-//       // it's null, and we'll pass the request to
-//       // fetch, which will use the network.
-//       return response || fetch(event.request);
-//     })
-//   );
+// // Here comes the install event!
+// // This only happens once, when the browser sees this
+// // version of the ServiceWorker for the first time.
+// self.addEventListener('install', function(event) {
+//     // We pass a promise to event.waitUntil to signal how 
+//     // long install takes, and if it failed
+//     event.waitUntil(// We open a cache…
+//     caches.open('simple-sw-v1').then(function(cache) {
+//         // And add resources to it
+//         return cache.addAll(['./index.html', './styles.css', "./logo-min.svg"]);
+//     }));
 // });
 
-self.addEventListener('fetch', function(event) {
-    event.respondWith(caches.open('simple-sw-v1').then(function(cache) {
-        return Promise.all([cache.match(event.request), fetch(event.request)])
-        .then(function(twoOptions) {
-            var cached = twoOptions[0]
-              , live = twoOptions[1];
-            if (live)
-                cache.put(event.request, live.clone());
-            return live || cached
-        })
+// // // The fetch event happens for the page request with the
+// // // ServiceWorker's scope, and any request made within that
+// // // page
+// // self.addEventListener('fetch', function(event) {
+// //   // Calling event.respondWith means we're in charge
+// //   // of providing the response. We pass in a promise
+// //   // that resolves with a response object
+// //   event.respondWith(
+// //     // First we look for something in the caches that
+// //     // matches the request
+// //     caches.match(event.request).then(function(response) {
+// //       // If we get something, we return it, otherwise
+// //       // it's null, and we'll pass the request to
+// //       // fetch, which will use the network.
+// //       return response || fetch(event.request);
+// //     })
+// //   );
+// // });
 
-    }));
-});
+// self.addEventListener('fetch', function(event) {
+//     event.respondWith(caches.open('simple-sw-v1').then(function(cache) {
+//         return Promise.all([cache.match(event.request), fetch(event.request)])
+//         .then(function(twoOptions) {
+//             var cached = twoOptions[0]
+//               , live = twoOptions[1];
+//             if (live)
+//                 cache.put(event.request, live.clone());
+//             return live || cached
+//         })
+
+//     }));
+// });

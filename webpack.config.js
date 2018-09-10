03580b3b9eebcb717dab21bd74b37f2db60f4e3a
Renan LE CARO <renan.lecaro@gmail.com>
2018-09-10 20:33:19 +0200

    Cleaner build

--- webpack.config.js
+++ webpack.config.js
@@ -84,7 +84,8 @@ module.exports = {
 
   plugins: [
     new HtmlWebpackPlugin({
-      template: "src/index.html", 
+      template: "src/index.html",  
+      filename:'../index.html'
     }),
     extractLess
   ]
